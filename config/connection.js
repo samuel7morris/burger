2a3d68b44d584b6cde56133e291bbbff1f35a2c2
Sam Morris <63416066+samuel7morris@users.noreply.github.com>
2020-08-03 09:08:42 -0600

    connected heroku server

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const util = require ("util");
 const mysql = require ("mysql");
-const connection = mysql.createConnection({
-    host: "localhost", 
-    user: "root", 
-    port: 3306, 
-    password: "root!", 
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost", 
+        user: "root", 
+        port: 3306, 
+        password: "root", 
+        database: "burgers_db"
+    })
+}
 
 connection.connect();
 
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,20 +1,20 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
+  $(".change-devoured").on("click", function(event) {
     var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
-
-    var newSleepState = {
-      sleepy: newSleep
+    var newDevoured = $(this).data("newdevoured");
+console.log(newDevoured)
+    var newDevourState = {
+      devoured: newDevoured
     };
 
     // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: newDevourState
     }).then(
       function() {
-        console.log("changed sleep to", newSleep);
+        console.log("changed sleep to", newDevoured);
         // Reload the page to get the updated list
         location.reload();
       }
@@ -25,18 +25,18 @@ $(function() {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newCat = {
-      name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+    var newBurger = {
+      burger_name: $("#ca").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
       type: "POST",
-      data: newCat
+      data: newBurger
     }).then(
       function() {
-        console.log("created new cat");
+        console.log("created new burger");
         // Reload the page to get the updated list
         location.reload();
       }
